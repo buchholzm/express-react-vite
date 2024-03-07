160aceb81b810a6463c26ce4acd300b4906a88c6
Michael Buchholz <mail@michael-buchholz.com>
2024-03-07 13:53:48 +0100

    feat: Adding queries

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
 import express from 'express';
 const router = express.Router();
+const db = new PrismaClient();
 
-router.get('/categories', (req, res) => {
-  res.json([]);
+router.get('/topics', async (req, res) => {
+  const topics = await db.topic.findMany();
+  res.json(topics);
+});
+
+router.get('/categories', async (req, res) => {
+  const categories = await db.category.findMany();
+  res.json(categories);
+});
+
+router.get('/bookmarks', async (req, res) => {
+  const bookmarks = await db.bookmark.findMany();
+  res.json(bookmarks);
 });
 
 router.all('*', (req, res) => {
